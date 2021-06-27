a2a7ccd313b2312600e543ca547ab8a48815eeaf
Harjit <harjitliyal@gmail.com>
2021-06-26 22:14:32 -0400

    Fixed oauth endpoint to include top artist/tracks

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -6,12 +6,22 @@ const Stats = () => {
     //Parses the querystring in the browser and store it into the session storage
     const hash = window.location.hash.substring(1).split("&")
     const accessToken = hash[0].substring(13)
-    console.log(accessToken)
     const storage = window.sessionStorage
     storage.setItem("accessToken", accessToken)
     console.log(storage.getItem('accessToken'))
+    const response = axios.get(`https://api.spotify.com/v1/me/top/artists`, {
+        headers: {
+            'Authorization': 'Bearer ' + accessToken,
+    }
+    })
+        .then(response => { 
+            console.log(response)
+        })
+        .catch(err => { 
+            console.log(err)
+        })
     
-    return (
+        return (
         <div>We made it to this page!
             
         </div>
