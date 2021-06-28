6f5d7de743de356833d0d8c6c50f6788ec2bcc47
brendangubbins <brendansg97@gmail.com>
2021-06-28 16:44:08 -0400

    recommendation playlist api

--- src/components/ArtistCard.js
+++ src/components/ArtistCard.js
@@ -3,10 +3,11 @@ import {
     Image,
     Text,
     Center,
-    Heading
+    Heading,
+    Button
 } from '@chakra-ui/react';
 
-export default function ArtistCard({ artist }) {
+export default function ArtistCard({ artist, artists, setArtists }) {
 
     const handleMouse = () => {
         console.log(`hovered ${artist.name}`)
@@ -14,6 +15,12 @@ export default function ArtistCard({ artist }) {
 
     const handleClick = (event) => {
         console.log(`clicked ${artist.name}`)
+    }
+
+    const removeArtist = (event) => {
+        event.preventDefault()
+        console.log(artists.filter(a => a.id !== artist.id))
+        setArtists(artists.filter(a => a.id !== artist.id))
     }
 
     return (
@@ -34,6 +41,7 @@ export default function ArtistCard({ artist }) {
                     maxHeight={'360px'}
                     src={artist.images[0].url}
                 />
+                <Button onClick={removeArtist}>Remove Artist</Button>
             </Box>
         </div>
     );
--- src/components/Discover.js
+++ src/components/Discover.js
@@ -2,15 +2,20 @@ import React, { useState, useEffect } from 'react';
 import ArtistCard from './ArtistCard';
 import axios from 'axios';
 import { SimpleGrid, Center } from '@chakra-ui/layout';
+import { Button } from '@chakra-ui/button';
 
 const Discover = () => {
     const [artists, setArtists] = useState([])
+    const [ playlist, setPlaylist ] = useState([])
 
     const storage = window.sessionStorage
     const accessToken = storage.getItem('accessToken')
 
     useEffect(() => {
         axios.get("https://api.spotify.com/v1/me/top/artists", {
+            params: {
+                'limit': 5
+            },
             headers: {
                 'Authorization': 'Bearer ' + accessToken,
             }
@@ -24,29 +29,49 @@ const Discover = () => {
             })
     }, [])
 
+    const generatePlaylist = (event) => {
+        event.preventDefault()
+        const artistIDs = artists.map(artist => artist.id)
+        console.log(artistIDs)
+        const seeds = artistIDs.join(",")
+        console.log(seeds)
+        axios.get("https://api.spotify.com/v1/recommendations", {
+            params: {
+                'seed_artists': seeds,
+                'limit': 20
+            },
+            headers: {
+                'Authorization': 'Bearer ' + accessToken,
+            }
+        })
+        .then(response => {
+            console.log(response.data.tracks)
+            setPlaylist(response.data.tracks)
+        })
+    }
+
     return (
         <div>
-            {/* {console.log(artists)} */}
-            Discover Page
+            <Center>
+                <Button onClick={generatePlaylist}>MAKE A PLAYLIST</Button>
+            </Center>
             <Center>
                 <SimpleGrid py={6} columns={3} spacingX="15px" spacingY="15px">
                     {artists.map(artist =>
                         <div key={artist.id}>
-                            {/* <li>
-                            <b>{artist.name}</b>
-                            <br></br>
-                            <img src={artist.images[2].url} alt={artist.name} />
-                            {artist.genres.map(genre => 
-                                <div key={genre}>
-                                    {genre}
-                                </div>  
-                            )}
-                        </li> */}
-                            <ArtistCard artist={artist} />
+                            <ArtistCard artist={artist} artists={artists} setArtists={setArtists} />
                         </div>
                     )}
                 </SimpleGrid>
             </Center>
+
+            {playlist.length ? (
+                    playlist.map(song => 
+                        <div key={song.id}>
+                            <p>{song.name}</p>
+                        </div>)
+                ) 
+            : null}
         </div>
     );
 }
