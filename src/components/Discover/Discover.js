4c410b84186327591f0ddb3989ca67b2beeda42d
brendangubbins <brendansg97@gmail.com>
2021-06-29 17:50:11 -0400

    fixed useEffect hooks for more responsive state updates

--- src/components/Discover/Discover.js
+++ src/components/Discover/Discover.js
@@ -1,15 +1,13 @@
 import React, { useState, useEffect, useContext } from 'react';
-import ArtistCard from './ArtistCard';
+import ArtistImage from './ArtistImage';
 import axios from 'axios';
-import { SimpleGrid, Center, Box, Container, Heading } from '@chakra-ui/layout';
+import { SimpleGrid, Center, Container, Heading } from '@chakra-ui/layout';
 import { Button } from '@chakra-ui/button';
 import AppContext from '../AppContext';
 import { Link } from 'react-router-dom';
 import { Slider, SliderFilledTrack, SliderThumb, SliderTrack } from '@chakra-ui/slider';
 
 const Discover = () => {
-    // const [artists, setArtists] = useState([])
-    // const [ playlist, setPlaylist ] = useState([])
 
     const [value, setValue] = useState(20)
     const handleChange = (value) => setValue(value)
@@ -28,28 +26,12 @@ const Discover = () => {
     } 
     = useContext(AppContext);
 
-    const storage = window.sessionStorage
-    const accessToken = storage.getItem('accessToken')
-
-    // useEffect(() => {
-    //     axios.get("https://api.spotify.com/v1/me/top/artists", {
-    //         params: {
-    //             'limit': 5
-    //         },
-    //         headers: {
-    //             'Authorization': 'Bearer ' + accessToken,
-    //         }
-    //     })
-    //         .then(response => {
-    //             console.log(response.data.items)
-    //             setArtists(response.data.items)
-    //         })
-    //         .catch(error => {
-    //             console.log(error)
-    //         })
-    // }, [])
-
+    // New recommended playlist on each 'artist' state update
     useEffect(() => {
+        const storage = window.sessionStorage
+        const accessToken = storage.getItem('accessToken')
+        console.log('state changed')
+        console.log(artist)
         const artistIDs = artist.map(a => a.artistId)
         const seeds = artistIDs.join(",")
         axios.get("https://api.spotify.com/v1/recommendations", {
@@ -62,49 +44,19 @@ const Discover = () => {
             }
         })
         .then(response => {
+            console.log(response.data.tracks)
             setUserPlaylist(response.data.tracks)
         })
-    }, [])
-
-    // const generatePlaylist = (event) => {
-    //     event.preventDefault()
-    //     const artistIDs = artist.map(a => a.id)
-    //     console.log(artistIDs)
-    //     const seeds = artistIDs.join(",")
-    //     console.log(seeds)
-    //     axios.get("https://api.spotify.com/v1/recommendations", {
-    //         params: {
-    //             'seed_artists': seeds,
-    //             'limit': 20
-    //         },
-    //         headers: {
-    //             'Authorization': 'Bearer ' + accessToken,
-    //         }
-    //     })
-    //     .then(response => {
-    //         console.log(response.data.tracks)
-    //         setUserPlaylist(response.data.tracks)
-    //     })
-    // }
+    }, [artist])
 
     return (
-        <div>
-            <div>
-
-                <Link to="/CreatePlaylist">
-                    <Button label="CreatePlaylist">
-                        MAKE A PLAYLIST
-                    </Button>
-                </Link>
-
-            </div>
-            
+        <div>   
             <div>
             <Center>
                 <SimpleGrid py={6} columns={3} spacingX="15px" spacingY="15px">
                     {artist.map(a =>
                         <div key={a.artistId}>
-                            <ArtistCard a={a} artists={artist} setArtists={setArtist} />
+                            <ArtistImage a={a} />
                         </div>
                     )}
                 </SimpleGrid>
@@ -116,9 +68,19 @@ const Discover = () => {
                 <SliderTrack bg="lightgreen">
                     <SliderFilledTrack bg="green" />
                 </SliderTrack>
-                <SliderThumb boxSize={6} children={value} />
+                <SliderThumb boxSize={6} textColor="black" children={value} />
             </Slider>
             </Container>
+            </div>
+
+            <div>
+                <Center>
+                <Link to="/CreatePlaylist">
+                    <Button label="CreatePlaylist">
+                        Generate A Playlist
+                    </Button>
+                </Link>
+                </Center>
             </div>
 
             {userPlaylist.length ? (
