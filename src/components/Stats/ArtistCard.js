4f10a0bd92d3b4d6138d6fd60a9ff88ca5726274
Harjit <harjitliyal@gmail.com>
2021-07-05 23:32:07 -0400

    revamped stats cards

--- src/components/Stats/ArtistCard.js
+++ src/components/Stats/ArtistCard.js
@@ -1,40 +1,96 @@
-import React from 'react';
-import { Box, Image, Flex, chakra } from '@chakra-ui/react';
+import { React, useState, useEffect } from 'react';
+import axios from 'axios';
+import { Box, Image, Flex, chakra, Popover, PopoverTrigger, PopoverContent, Center, Spinner, PopoverHeader, PopoverBody, Text, AlertTitle } from '@chakra-ui/react';
 
 // Returns a artist card with the artist image & name
 const ArtistCard = ({ data, ranking }) => {
+    const [artistTopTracks, setArtistTopTracks] = useState([])
+    const token = window.sessionStorage.getItem('accessToken')
+
+    useEffect(() => {
+        axios.get(`https://api.spotify.com/v1/artists/${data.artistId}/top-tracks?market=US`, {
+            headers: {
+                'Authorization': 'Bearer ' + token,
+            }
+        })
+            .then(response => {
+                const topArr = []
+                for (let i = 0; i < 3; i++) {
+                    topArr.push(response.data.tracks[i])
+                }
+                console.log(response)
+                setArtistTopTracks(topArr)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }, [])
+
+    while (typeof (artistTopTracks) === 'undefined') {
+        return (
+            <Center pt={20} pb={20}>
+                <Spinner size="xl" color='green'>
+                </Spinner>
+            </Center>
+        )
+    }
     return (
         <Flex
             direction="column"
             justifyContent="center"
             alignItems="center"
             w="sm"
-            mx="auto"
+            // mx="auto"
+            cursor='pointer'
         >
-            <Box
-                // _hover={{
-                //     background: 'black',
-                //     opacity: '90%',
-                //     zindex: '10',
-                //     transition: '0.1s ease-in',
-                //     rounded:"lg",
-                // }}
+            <Popover
+                placement='right'
+                boxShadow = 'none'
             >
-            <Image
-                src={`${data.artistImage}`}
-                rounded="lg"
-                shadow="md"
-                bgSize="cover"
-                bgPos="center"
-                draggable='false'
-                // _hover={{
-                //     background: 'black',
-                //     opacity: '0.13',
-                //     zindex: '10',
-                //     transition: '0.3s'
-                // }}
-            />
-            </Box>
+
+                <PopoverTrigger boxShadow = 'none'>
+                    <Image
+                        src={`${data.artistImage}`}
+                        rounded="lg"
+                        boxShadow="md"
+                        bgSize="cover"
+                        bgPos="center"
+                        draggable='false'
+                    />
+                </PopoverTrigger>
+                <PopoverContent
+                    width='sm'
+                    fontWeight='semibold'
+                    bgGradient="linear(to-l, #fffcff, #d5fefd)"
+                    boxShadow = 'none'
+                >
+                    <PopoverBody boxShadow = 'none'>
+                        <Center>
+                            <Text fontWeight='bold'>
+                        {data.artistName}'s Top Tracks
+                        </Text>
+                        </Center>
+                    </PopoverBody>
+                    
+                    <PopoverBody border = 'none' textAlign='center'>
+                        <>
+                            {artistTopTracks.map((title, index) =>
+                                <li key={title.name}>
+                                    #{index + 1} {title.name}
+                                </li>
+
+                            )}
+                        </>
+                    </PopoverBody>
+                    <Center>
+                        <PopoverBody>
+                        Followers : {data.artistFollowers.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+                        </PopoverBody>
+
+                    </Center>
+                </PopoverContent>
+            </Popover>
+
             <Box
                 w={{ base: 56, md: 64 }}
                 bgColor='lightgray'
@@ -45,11 +101,9 @@ const ArtistCard = ({ data, ranking }) => {
             >
                 <chakra.h3
                     py={2}
-                    // fontFamily='Montserrat'
                     textAlign="center"
                     fontWeight="bold"
                     bgGradient="linear(to-l, #ffffff, #d7e1ec)"
-                    // background-image: linear-gradient(315deg, #ffffff 0%, #d7e1ec 74%);
                     textAlign="center"
                     fontSize='15px'
                 >
@@ -59,24 +113,38 @@ const ArtistCard = ({ data, ranking }) => {
                 </chakra.h3>
             </Box>
         </Flex>
-        
-        );
-    }
+
+    );
+
+}
 export default ArtistCard;
-        
-        // <Box maxW="sm" maxH='lg' borderWidth="1px" borderRadius="lg" overflow="hidden" bgGradient="linear(to-l, #01baef, #20bf55)">
+
+        // <Box maxW="sm" maxH='lg' borderWidth="1px" borderRadius="lg" overflow="hidden" bgGradient="linear(to-l, #01baef, #20bf55) // _hover={{
+        //     background: 'black',
+        //     opacity: '0.13',
+        //     zindex: '10',
+        //     transition: '0.3s'
+        // }}">
         //     <Image src={`${data.artistImage}`} draggable='false' />
         //     <Center pt={3}>
         //         <Badge
         //             colorScheme="white"
         //             variant="solid"
         //             fontSize="30px"
-                    
+
         //         >
         //             #{ranking}
         //         </Badge>
         //     </Center>
-        //     <Box d="flex" p='6' justifyContent='center' alignItems='center'>
+        //     <Box d="flex" p='6' justifyContent='center' alignItems='center'
+        //_hover={{
+        //     background: 'black',
+        //     opacity: '90%',
+        //     zindex: '10',
+        //     transition: '0.1s ease-in',
+        //     rounded:"lg",
+
+        //}}>
         //         <Text
         //             fontSize='20px'
         //             textColor='white'
