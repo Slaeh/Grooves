6afe8fda05ca6fb7d7f1da709b68c3daddb31770
Edward Paez <paez.edward5@gmail.com>
2021-07-07 23:30:21 -0400

    Add styling for Closing and Playlist page. Format code with Prettier extension. Add responsive styling for Discover page and add Logout functionality.

--- src/components/Playlist/Song.js
+++ src/components/Playlist/Song.js
@@ -1,69 +1,94 @@
-import { Text } from "@chakra-ui/react"
-import { Divider } from "@chakra-ui/react"
-import { IconButton } from "@chakra-ui/react"
-import { FaTrash } from 'react-icons/fa'
-import { Center } from "@chakra-ui/react"
-import { Image, Box } from "@chakra-ui/react"
-import { Flex, Spacer } from "@chakra-ui/react"
+import { Text } from '@chakra-ui/react';
+import { Divider } from '@chakra-ui/react';
+import { IconButton } from '@chakra-ui/react';
+import { FaTrash } from 'react-icons/fa';
+import { Center } from '@chakra-ui/react';
+import { Image, Box } from '@chakra-ui/react';
+import { Flex, Spacer } from '@chakra-ui/react';
 
 const Song = ({ onClick, image, title, artist, album, year, previewAudio }) => {
-    let audio = new Audio()
-    return ( 
+  let audio = new Audio();
+  return (
     <>
-    <Flex p="10px"align="center">
-        {
-            previewAudio != null ?
-            (
-                <Image
-                src={image.url} 
-                alt="album"
-                boxSize={['50px', '79px']}
-                draggable="false"
-                onMouseOver={() => {
-                    console.log('hello')
-                    audio = new Audio(previewAudio)
-                    audio.play()
-                }}
-                onMouseOut={() => {
-                    console.log('bye')
-                    audio.pause()
-                }}
-                />
-            )
-            :
-            (
-                <Image
-                src={image.url} 
-                alt="album"
-                boxSize={['50px', '79px']}
-                draggable="false"
-                opacity="25%"
-                />
-            )
-        }
-        <Box m={['5px', '5px']}width='100px' textAlign="left"><Text fontWeight="bold" fontSize={['11px', '14px']}>{title}</Text></Box>
-        <Spacer />
-        <Box  m={['5px', '5px']}width="100px" textAlign="center">
-            <Text fontWeight="bold" fontSize={['11px', '14px']}>{artist}</Text>
+      <Flex p="10px" align="center">
+        {previewAudio != null ? (
+          <Image
+            src={image.url}
+            alt="album"
+            boxSize={['50px', '79px']}
+            draggable="false"
+            onMouseOver={() => {
+              console.log('hello');
+              audio = new Audio(previewAudio);
+              audio.play();
+            }}
+            onMouseOut={() => {
+              console.log('bye');
+              audio.pause();
+            }}
+          />
+        ) : (
+          <Image
+            src={image.url}
+            alt="album"
+            boxSize={['50px', '79px']}
+            draggable="false"
+            opacity="25%"
+          />
+        )}
+        <Box m={['5px', '5px']} width="100px" textAlign="left">
+          <Text fontWeight="bold" fontSize={['11px', '14px']}>
+            {title}
+          </Text>
         </Box>
         <Spacer />
-        <Box  m={['5px', '5px']}width="100px" textAlign="center">
-            <Text fontWeight="bold" fontSize={['11px', '14px']}>{album}</Text>
+        <Box m={['5px', '5px']} width="100px" textAlign="center">
+          <Text fontWeight="bold" fontSize={['11px', '14px']}>
+            {artist}
+          </Text>
         </Box>
         <Spacer />
-        <Box  d={['none', 'block']}m={['5px', '5px']}width="100px" textAlign="center">
-            <Text fontWeight="bold" fontSize={['11px', '14px']}>{year}</Text>
+        <Box m={['5px', '5px']} width="100px" textAlign="center">
+          <Text fontWeight="bold" fontSize={['11px', '14px']}>
+            {album}
+          </Text>
         </Box>
         <Spacer />
-        <Box  mr={['5px', '5px']}textAlign={['right', 'right', 'right', 'right', 'left']}width="100px">
-            <IconButton size={['sm', 'md']}variant="unstyled" onClick={onClick} bg="#e6e6e6"icon={<FaTrash />}/>
+        <Box
+          d={['none', 'block']}
+          m={['5px', '5px']}
+          width="100px"
+          textAlign="center"
+        >
+          <Text fontWeight="bold" fontSize={['11px', '14px']}>
+            {year}
+          </Text>
         </Box>
-    </Flex>
-    <Center>
-        <Divider size="50px"width="90%"borderColor="gray.500" orientation="horizontal"/>
-    </Center>
+        <Spacer />
+        <Box
+          mr={['5px', '5px']}
+          textAlign={['right', 'right', 'right', 'right', 'left']}
+          width="100px"
+        >
+          <IconButton
+            size={['sm', 'md']}
+            variant="unstyled"
+            onClick={onClick}
+            bg="#e6e6e6"
+            icon={<FaTrash />}
+          />
+        </Box>
+      </Flex>
+      <Center>
+        <Divider
+          size="50px"
+          width="90%"
+          borderColor="gray.500"
+          orientation="horizontal"
+        />
+      </Center>
     </>
-    )
-}
+  );
+};
 
-export default Song
+export default Song;
