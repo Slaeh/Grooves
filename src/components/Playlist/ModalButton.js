8b04a06599918963443bc139c0652d2e689d7b2c
Edward Paez <paez.edward5@gmail.com>
2021-07-09 22:56:06 -0400

    Test 5

--- src/components/Playlist/ModalButton.js
+++ src/components/Playlist/ModalButton.js
@@ -2,16 +2,14 @@ import { Button } from '@chakra-ui/react';
 
 const ModalButton = ({ handleCreatePlaylist }) => {
   return (
-    <a href="https://grooves-app.herokuapp.com/Closing/">
-      <Button
-        onClick={handleCreatePlaylist}
-        size="lg"
-        mt="20px"
-        colorScheme="green"
-      >
-        Create
-      </Button>
-    </a>
+    <Button
+      onClick={handleCreatePlaylist}
+      size="lg"
+      mt="20px"
+      colorScheme="green"
+    >
+      Create
+    </Button>
   );
 };
 
