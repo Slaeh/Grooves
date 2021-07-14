7ccc3e2ea75914200ccd7d8e7279fd3db25b915c
Edward Paez <paez.edward5@gmail.com>
2021-07-14 18:59:14 -0400

    Fix Footer and Navbar spacing

--- src/components/Footer/LinkGrid.js
+++ src/components/Footer/LinkGrid.js
@@ -1,87 +1,109 @@
-import { Box, Link, SimpleGrid, Stack, Center } from "@chakra-ui/react";
-import * as React from "react";
-import { FooterHeading } from "./FooterHeading";
-import { SocialMediaLinks } from "./SocialMediaLinks";
+import {
+  Box,
+  Link,
+  SimpleGrid,
+  Stack,
+  Center,
+  Flex,
+  Spacer,
+} from '@chakra-ui/react';
+import * as React from 'react';
+import { FooterHeading } from './FooterHeading';
+import { SocialMediaLinks } from './SocialMediaLinks';
 // import About from "../About";
 
 // Links in footer
 export const LinkGrid = (props) => (
-  <SimpleGrid columns={3} {...props}>
-    <Box minW="130px">
+  <Flex w="100%" justify={['center', 'flex-end']}>
+    <Flex flexFlow="column wrap" mr={['10px', '0', '30px', '100px']}>
       {/* Application portion of footer - contains About & Contact Information */}
+      {/* <Center> */}
+      <FooterHeading mb="4" textColor="#919496" fontWeight="bold">
+        Application
+      </FooterHeading>
+      {/* </Center> */}
+      {/* <Stack>
+          <Center> */}
       <Center>
-        <FooterHeading mb="4" textColor="#919496" fontWeight="bold">
-          Application
-        </FooterHeading>
+        <Link
+          href="/About"
+          target="_blank"
+          textColor="white"
+          _hover={{ textColor: '#1DB954' }}
+        >
+          About
+        </Link>
       </Center>
-      <Stack>
-        <Center>
-          <Link
-            href="/About"
-            target="_blank"
-            textColor="white"
-            _hover={{ textColor: "#1DB954" }}
-          >
-            About
-          </Link>
-        </Center>
-        {/* <Center>
+      {/* </Center> */}
+      {/* <Center>
           <Link textColor="white" _hover={{ textColor: "#1DB954" }}>
             Contact
           </Link>
         </Center> */}
-      </Stack>
-    </Box>
-    <Box minW="130px">
+      {/* </Stack> */}
+    </Flex>
+    <Flex
+      flexFlow="column wrap"
+      justify="space-between"
+      mr={['10px', '0', '30px', '100px']}
+    >
       {/* Application portion of footer - contains About & Contact Information */}
       <Center>
         <FooterHeading mb="4" textColor="#919496" fontWeight="bold">
           Made By
         </FooterHeading>
       </Center>
-      <Stack>
-        <Center>
-          <Link
-            href="https://github.com/brendangubbins"
-            target="_blank"
-            textColor="white"
-            _hover={{ textColor: "#1DB954" }}
-          >
-            Brendan Gubbins
-          </Link>
-        </Center>
-        <Center>
-          <Link
-            href="https://github.com/Slaeh"
-            target="_blank"
-            textColor="white"
-            _hover={{ textColor: "#1DB954" }}
-          >
-            Harjit Liyal
-          </Link>
-        </Center>
-        <Center>
-          <Link
-            href="https://github.com/epaez1996"
-            target="_blank"
-            textColor="white"
-            _hover={{ textColor: "#1DB954" }}
-          >
-            Edward Paez
-          </Link>
-        </Center>
-      </Stack>
-    </Box>
-    <Box minW="130px">
-      {/* Source Code portion of footer - contains link to GitHub repo */}
+      {/* <Stack> */}
       <Center>
+        <Link
+          href="https://github.com/brendangubbins"
+          target="_blank"
+          textColor="white"
+          _hover={{ textColor: '#1DB954' }}
+          mb="5px"
+          w={[null, null, '200px', null]}
+          textAlign="center"
+        >
+          Brendan Gubbins
+        </Link>
+      </Center>
+      <Center>
+        <Link
+          href="https://github.com/Slaeh"
+          target="_blank"
+          textColor="white"
+          _hover={{ textColor: '#1DB954' }}
+          mb="5px"
+        >
+          Harjit Liyal
+        </Link>
+      </Center>
+      <Center>
+        <Link
+          href="https://github.com/epaez1996"
+          target="_blank"
+          textColor="white"
+          _hover={{ textColor: '#1DB954' }}
+          mb="5px"
+        >
+          Edward Paez
+        </Link>
+      </Center>
+      {/* </Stack> */}
+    </Flex>
+
+    <Flex flexDir="column">
+      <Box>
+        {/* Source Code portion of footer - contains link to GitHub repo */}
+        {/* <Center> */}
         <FooterHeading mb="4" textColor="#919496" fontWeight="bold">
           Source Code
         </FooterHeading>
-      </Center>
-      <Center>
-        <SocialMediaLinks />
-      </Center>
-    </Box>
-  </SimpleGrid>
+        {/* </Center> */}
+        <Center>
+          <SocialMediaLinks />
+        </Center>
+      </Box>
+    </Flex>
+  </Flex>
 );
