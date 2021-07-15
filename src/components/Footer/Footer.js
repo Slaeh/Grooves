ae27dd2a009af206ab4d089c8dba28a5ca36c570
Edward Paez <paez.edward5@gmail.com>
2021-07-14 22:17:17 -0400

    Change font size for home page headings on mobile devices

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,8 +1,8 @@
-import { Box, Flex, Spacer, Center } from "@chakra-ui/react";
-import * as React from "react";
-import { LinkGrid } from "./LinkGrid";
-import { Logo } from "./Logo";
-import { motion } from "framer-motion";
+import { Box, Flex, Spacer, Center } from '@chakra-ui/react';
+import * as React from 'react';
+import { LinkGrid } from './LinkGrid';
+import { Logo } from './Logo';
+import { motion } from 'framer-motion';
 
 const Footer = () => {
   return (
@@ -12,19 +12,19 @@ const Footer = () => {
       bg="black"
       bottom="0"
       alignItems="center"
-      direction={["column", "column", "row", "row"]}
-      p={["0", "35px"]}
+      direction={['column', 'column', 'row', 'row']}
+      p={['0', '35px']}
     >
       <Center>
         <Box
           // pl={20}
           // pr={20}
-          mt={["25px", "25px", "0px"]}
-          mb={["30px", "0"]}
+          mt={['25px', '25px', '0px']}
+          mb={['30px', '0']}
           // mr="10px"
         >
           <motion.button whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
-            <a href="https://spotify.com/" target="_blank">
+            <a href="https://grooves-app.herokuapp.com/" target="_blank">
               <Center>
                 <Logo />
               </Center>
@@ -40,9 +40,9 @@ const Footer = () => {
         pt={10}
         pb={5}
         spacing={{
-          base: "10",
-          md: "20",
-          lg: "28",
+          base: '10',
+          md: '20',
+          lg: '28',
         }}
       />
     </Flex>
